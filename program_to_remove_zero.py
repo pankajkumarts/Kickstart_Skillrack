f9b05ea9e8b2f175540ebf8f5225200ea5b8be20
PANKAJ KUMAR TS <pankaj.sai32@gmail.com>
2018-12-23 12:32:48 +0530

    Update program_to_remove_zero.py

--- program_to_remove_zero.py
+++ program_to_remove_zero.py
@@ -1,9 +1,9 @@
-## **REVERSE A NUMBER**
-#Number N is passsed as input.program must print reverse number of N
-#input1: 2541
-#output1:1425
-#input2:6500
-#output2:56
+# **REVERSE A NUMBER**
+## Number N is passsed as input.program must print reverse number of N
+## input1: 2541
+## output1:1425
+## input2:6500
+## output2:56
 
 num = int(input())
 while num%10 == 0 and num!=0:
